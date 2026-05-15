3a58349a8626f77f7137f19f400484d40045f1d4
Carlos <robercarlosleyvva@gmail.com>
2026-05-15 15:57:43 -0600

    Modifique la clase Pokemon.java dandole atributos, constructor, metodos y getters

--- app/src/main/java/model/Jugador.java
+++ app/src/main/java/model/Jugador.java
@@ -1,5 +1,30 @@
 package model;
+import java.util.ArrayList;
 
 public class Jugador {
+    //Atributos
+    private String nombre;
+    private ArrayList<Pokemon>  equipo;
+
+    //Constructor
+
+    public Jugador (String nombre){
+        this.nombre = nombre;
+        
+        equipo = new ArrayList<>();
+    }
+
+    //Metodos
+    public void agregarPokemon(Pokemon pokemon){
+        equipo.add(pokemon);
+    }
+
+    //Getter
+    public ArrayList<Pokemon> getEquipo() {
+
+    return equipo;
+}
+
+
     
 }
--- app/src/main/java/model/Pokemon.java
+++ app/src/main/java/model/Pokemon.java
@@ -2,9 +2,11 @@ package model;
 
 public class Pokemon {
 
+    //ATRIBUTOS
     private String nombre;
     private int nivel;
     private int vida;
+    private int vidaMaxima;
     private int ataque;
     private int defensa;
 
@@ -14,16 +16,71 @@ public class Pokemon {
     public Pokemon(String nombre,
                    int nivel,
                    int vida,
+                   int vidaMaxima,
                    int ataque,
                    int defensa,
-                   TipoPokemon tipo) {
+                   TipoPokemon tipo){
 
         this.nombre = nombre;
         this.nivel = nivel;
         this.vida = vida;
+        this.vidaMaxima= vidaMaxima;
         this.ataque = ataque;
         this.defensa = defensa;
         this.tipo = tipo;
+        }
+
+    //Getters
+
+    public String getNombre(){
+        return nombre;
+    }
+    public int getNivel (){
+        return nivel;
+    }
+    public int getVida(){
+        return vida;
+    }
+    public int getAtaque(){
+        return ataque;
+    }
+    public int getDefensa(){
+        return defensa;
+    }
+    public TipoPokemon getTipo(){
+        return tipo;
     }
     
+   //METODOS
+   // recibir danio
+    public void recibirDanio(int danio) {
+
+    vida -= danio;
+
+    if (vida < 0) {
+        vida = 0;
+    }
+    //Verificar si esta debilitado true o false
+    }
+    public boolean estaDebilitado (){
+        return vida <=0;
+    }
+    //Curar al pokemon
+    public void  curar(int cantidad){
+        vida += cantidad;
+
+        if (vida > vidaMaxima){
+            vida = vidaMaxima;
+
+        }
+    }
+    //Subir de Nivel, se mejoran las stats
+    public void subirNivel(){
+        nivel++;
+
+        vidaMaxima +=10;
+        ataque+=2;
+        defensa +=2;
+        vida +=vidaMaxima;
+    }
 }
