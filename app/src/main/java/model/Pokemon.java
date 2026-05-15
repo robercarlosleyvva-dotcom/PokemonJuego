bb8b224264f541e2d6070e161c6e51c9af528665
Carlos <robercarlosleyvva@gmail.com>
2026-05-15 14:06:52 -0600

    Agregue el constructor

--- app/src/main/java/model/Pokemon.java
+++ app/src/main/java/model/Pokemon.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Pokemon {
+
+    private String nombre;
+    private int nivel;
+    private int vida;
+    private int ataque;
+    private int defensa;
+
+    private TipoPokemon tipo;
+
+    // CONSTRUCTOR
+    public Pokemon(String nombre,
+                   int nivel,
+                   int vida,
+                   int ataque,
+                   int defensa,
+                   TipoPokemon tipo) {
+
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.tipo = tipo;
+    }
     
 }
